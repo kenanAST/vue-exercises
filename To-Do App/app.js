c9a9b6dc00d9f17f61ba3cc9eb77cff986082fb6
kenannn <kenanjoshua3454@gmail.com>
2021-09-21 23:37:09 +0800

    to-do-app

---
+++ To-Do App/app.js
@@ -0,0 +1,16 @@
+Vue.createApp({
+	data() {
+		return {
+			taskList: [],
+			taskInput: '',
+		};
+	},
+	methods: {
+		addTask() {
+			this.taskList.push(this.taskInput);
+		},
+		deleteTask(task) {
+			this.taskList.splice(this.taskList.indexOf(task), 1);
+		},
+	},
+}).mount('#app');
