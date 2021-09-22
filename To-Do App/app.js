a68d08e8a1d30e9dc16fbdf7a377b06bd919d9a0
kenannn <kenanjoshua3454@gmail.com>
2021-09-22 09:09:59 +0800

    Update app.js

--- To-Do App/app.js
+++ To-Do App/app.js
@@ -8,6 +8,7 @@ Vue.createApp({
 	methods: {
 		addTask() {
 			this.taskList.push(this.taskInput);
+			this.taskInput = '';
 		},
 		deleteTask(task) {
 			this.taskList.splice(this.taskList.indexOf(task), 1);
