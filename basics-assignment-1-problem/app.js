e173986ec56a1d1c57ad1fade81ddff564871c12
kenannn <kenanjoshua3454@gmail.com>
2021-09-23 16:24:49 +0800

    Added assignment 1

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+app = Vue.createApp({
+	data() {
+		return {
+			name: 'Joshua Kenan S. Cinches',
+			age: 20,
+			imageUrl: 'https://cdn.discordapp.com/attachments/688591700234469394/890269359174484048/IMG_163179263423F.jpg',
+		};
+	},
+});
+
+app.mount('#assignment');
