589cdf3e3e97802fc9251a27d50446d06d2fff66
kenannn <kenanjoshua3454@gmail.com>
2021-09-25 22:29:35 +0800

    Native Event Object

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,10 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      username: '',
     };
   },
 
   methods: {
+
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName ;
+    },
     
     add(num){
       this.counter += num;
