e9689e982ba4926915bf3d95aaa0d1dea968099f
kenannn <kenanjoshua3454@gmail.com>
2021-09-25 22:02:40 +0800

    exercise 3

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    
+    add(){
+      this.counter++;
+    },
+
+    subtract(){
+      this.counter--;
+    }
+
+  }
+
+});
+
+app.mount('#events');
