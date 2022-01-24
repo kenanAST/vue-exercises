f64689b5c279316b3b9a97d92da6e0d7611b1442
kenannn <kenanjoshua3454@gmail.com>
2022-01-24 14:22:58 +0800

    Style Scope and Slots

---
+++ 2.1 cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+	margin: 2rem auto;
+	max-width: 30rem;
+	border-radius: 12px;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+	padding: 1rem;
+}
+
+section div {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.badge--admin {
+	background-color: #810036;
+	color: white;
+}
+
+.badge--author {
+	background-color: #002c8a;
+	color: white;
+}
+</style>
